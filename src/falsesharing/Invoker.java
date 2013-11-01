2f042d83c0960c48fe2070b6f6fc3eb47a1658c3
nrpndr <nripendra.thakur87@gmail.com>
2013-11-01 08:33:24 +0530

    false Sharing

---
+++ src/falsesharing/Invoker.java
@@ -0,0 +1,17 @@
+package falsesharing;
+
+public class Invoker {
+	
+	public static void main(String[] args) throws Exception {
+		FalseSharing.runtest();
+		System.out.println("===================");
+		FalseSharing.runtest();
+		System.out.println("===================");
+		FalseSharing.runtest();
+		System.out.println("===================");
+		FalseSharing.runtest();
+		System.out.println("===================");
+		FalseSharing.runtest();
+	}
+
+}
